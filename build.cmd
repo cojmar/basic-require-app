4a6cbe2b3ef802618518f1afeb0d1a629975fdf1
cojmar <cojmar@gmail.com>
2019-07-26 06:12:22 +0300

    2nd commit

--- build.cmd
+++ build.cmd
@@ -1,10 +1,12 @@
 @ECHO OFF
 @rmdir build /s /q
-node tools/r.js -o tools/build.js
-@ECHO OFF
+@node tools/r.js -o tools/build.js
 @copy "build\scripts\main.js" "build\main.js"
 @rmdir "build\scripts" /s /q
 @rmdir "build\lib" /s /q
+@rmdir "build\assets" /s /q
 @del  "build\build.txt"
+@del  "build\index.html"
+@ren  "build\index.build.html" "index.html"
 
 @ECHO --------- build done ----------
