bbd4a75c1512f972cc057bbff5cf5f826c9d6baf
cojmar <cojmar@gmail.com>
2019-07-27 05:48:20 +0300

    polish

--- src/app/main.js
+++ src/app/main.js
@@ -1,25 +1,16 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var messages = require('./messages');    
-    // Load library/vendor modules using
-    // full IDs, like:
-    var print = require('print');
-    
-
-    require('jquery');
     require('css!assets/css/main');
-
-    var obj ={
-        init:function(){   
-            print(messages.getHello());
-            $('body').append(messages.getHello());
+    var $ = require('jquery');    
+    var app ={
+        messages:require('./messages'),
+        print:require('print'),
+        init:function(){               
+            $('body').append(app.messages.getHello());
+            app.print('App Init Ok');
         }
     };
     $(function(){
-        obj.init();
-    })    
-    return obj;
-
+        app.init();
+        //app.print(require('text!assets/css/main.css'));
+    })        
 });
